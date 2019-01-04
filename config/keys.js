e3f3bc0047c2041d9d63e40740088535f3d67efa
optimized faisal <optimizedfaisal42@gmail.com>
2019-01-04 21:53:26 +0600

    Uploaded to heroku

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 //mongodb theke je connection string ta paici ota ekhane store korbo
 
 module.exports = {
-    MongoURI: 'mongodb+srv://faisal4590:faisal4590@firstcluster-y8get.mongodb.net/test?retryWrites=true'
+    MongoURI: 'mongodb://faisal4590:faisal4590@ds149404.mlab.com:49404/login-reg-node-and-passport'
 }
